6317b5566650c5ddc609cdc80175d29d17af0fbf
claudiaalozano <clau.lozano.perez@gmail.com>
2022-11-24 19:44:50 +0100

    función para obter el nodo con menor prob

--- ejercicio1.py
+++ ejercicio1.py
@@ -57,3 +57,44 @@ class Huffman:
             self.prob[simbolo] = prob[simbolo]
 
     def construir_arbol(self):
+        ind_min1 = self.get_Nodo_minp()
+        ind_min2 = self.get_Nodo_minp()
+
+        while ind_min1!= -1 and ind_min2!= -1:
+            nodo = Nodo()
+            nodo.simbolo = "."
+            nodo.codificacion = ""
+
+            prob1 = self.nodos[ind_min1].probabilidad
+            prob2 = self.nodos[ind_min2].probabilidad
+            nodo.probabilidad = prob1 + prob2
+            nodo.visited = False
+            nodo.longitud = -1
+            self.nodos.append(nodo)
+            self.nodos[ind_min1].longitud = len(self.nodos)-1
+            self.nodos[ind_min2].longitud = len(self.nodos)-1
+
+            #comparaciones
+            if prob1 >= prob2:
+                self.nodos[ind_min1].codificacion = 0
+                self.nodos[ind_min2].codificacion = 1
+
+            ind_min1 = self.get_Nodo_minp()
+            ind_min2 = self.get_Nodo_minp()
+
+        def obtener_nodo_menor_prob(self):
+            minp= 1.0
+            index_min = -1
+
+            for index in range ( 0, len(self.nodos)):
+                if (self.nodos[index].probabilidad < minp and (not self.nodos[index].visited)):
+                    minp = self.nodos[index].probabilidad
+                    index_min = index
+
+                elif index_min != -1:
+                    self.nodos[index_min].visited = True
+
+            return index_min
+
+            
+
